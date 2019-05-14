2bd5b51ea7b37e9f342086b0e60da07fa46fe5b4
TheCakeChicken <33702174+TheCakeChicken@users.noreply.github.com>
2019-05-14 12:21:45 +0100

    Update Electra.lua

--- src/Server/Electra.lua
+++ src/Server/Electra.lua
@@ -2,8 +2,22 @@
 local server = {
     Root = script.Parent.Parent;
 }
-local service = require(server.Root.Server.Modules.Service)
+local service = require(server.Root.Server.Modules.Service) --// the only file we will ever, ever manually require; it's functions are needed before the main modules are loaded.
 
 return service.NewProxy("Electra_Core", function(data)
-
+    server.LoadOrder = {
+            "Electra/Processing";
+            "Electra/Events";
+            "Electra/DataStore";
+            "Electra/Core";
+            "Electra/AE";
+            "Electra/API";
+            "Dependencies/Settings";
+            "Dependencies/Meta";
+            "Dependencies/Credits";
+    }
+        
+    for _,ModuleName in next,server.LoadOrder do
+            local Split = service.Strings.Split(ModuleName)
+    end
 end)

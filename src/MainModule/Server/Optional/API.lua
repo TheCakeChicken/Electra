e55cf948bc3439d40f8d195900ea49d54d80fe62
Terryiscool160 <48654513+Terryiscool160@users.noreply.github.com>
2020-06-03 23:58:10 +0100

    plan

--- src/MainModule/Server/Optional/API.lua
+++ src/MainModule/Server/Optional/API.lua
@@ -3,7 +3,11 @@ service = nil
 
 return function()
     server.API = {
-
+     --// Ideas: 
+     --// API For checking a user is a admin
+     --// API For checking a user is a banned
+     --// API For getting trello data?
+     --// API For the datastore (only if enabled in settings)
     }
 
     if server.Settings.API.Enabled then

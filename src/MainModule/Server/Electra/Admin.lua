c42ede241afcd138c505321b96f5b219e7620dd4
Terryiscool160 <48654513+Terryiscool160@users.noreply.github.com>
2020-06-12 17:37:08 +0100

    Update Admin.lua

--- src/MainModule/Server/Electra/Admin.lua
+++ src/MainModule/Server/Electra/Admin.lua
@@ -2,15 +2,17 @@ server = nil
 service = nil
 
 return function()
+	
+	local Levels = {
+       ["-1"] = "Banned", 
+       ["0"] = "Player", 
+       ["1"] = "Moderator",
+       ["2"] = "Admin", 
+       ["3"] = "Creator",
+	}
+	
     server.Admin = {
 
-        local Levels = {
-            ["-1"] = "Banned", 
-            ["0"] = "Player", 
-            ["1"] = "Moderator",
-            ["2"] = "Admin", 
-            ["3"] = "Creator",
-        }
 
     }
 end

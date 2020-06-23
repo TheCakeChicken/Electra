4e6d7e859cbc33267cc30680641f0e2c32ac4ef0
MrTech999 <48654513+MrTech999@users.noreply.github.com>
2020-06-23 14:41:56 +0100

    credits

--- src/MainModule/Server/Dependencies/Credits.lua
+++ src/MainModule/Server/Dependencies/Credits.lua
@@ -3,5 +3,10 @@ service = nil
 
 return function()
     server.Credits = {
+        {Text = "Terryiscool160", Desc = "Developer of Electra."};
+        {Text = "ShipMaster_Caine", Desc = "Developer of Electra."};
+        {Text = "TheCakeChicken", Desc = "Original Developer, wrote Electra's base code."};
+        {Text = "FuginCZ", Desc = "Helped me fix my stupid mistakes with stuff, one of the coolest people I know (along with Cake)."};
+        {Text = "Sceleratis/Davey_Bones", Desc = "Wrote the window API which Electra uses, super useful."};
     }
 end
